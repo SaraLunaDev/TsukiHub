5c0cd7ced9f18368d01c59aa80522b294e1b6738
SaraLunaDev <blacktsukibiz@gmail.com>
2024-11-26 00:24:10 +0100

    Pagina TTS

--- src/components/TTS/TTS.js
+++ src/components/TTS/TTS.js
@@ -6,27 +6,61 @@ function TTS() {
   const [sounds, setSounds] = useState([]);
   const [currentAudio, setCurrentAudio] = useState(null);
   const [textEditorContent, setTextEditorContent] = useState("");
+  const [creditsPercentage, setCreditsPercentage] = useState(null);
   const [characterLimit] = useState(500);
 
   const fetchCredits = async () => {
+    const apiUrls = [
+      {
+        url: "https://api.elevenlabs.io/v1/user/subscription",
+        key: process.env.REACT_APP_ELEVENLABS_API_1,
+      },
+      {
+        url: "https://api.elevenlabs.io/v1/user/subscription",
+        key: process.env.REACT_APP_ELEVENLABS_API_2,
+      },
+    ];
+
     try {
-      const response = await fetch("/api/credits");
-      const data = await response.json();
-      return data.availablePercentage || 0;
+      let totalCredits = 0;
+      let totalLimits = 0;
+
+      for (const { url, key } of apiUrls) {
+        const response = await fetch(url, {
+          headers: { "xi-api-key": key },
+        });
+
+        if (!response.ok) {
+          console.error(`Error con la API Key: ${key}`);
+          continue;
+        }
+
+        const data = await response.json();
+        totalCredits += data.character_count || 0;
+        totalLimits += data.character_limit || 0;
+      }
+
+      // Calcula el porcentaje disponible
+      const availablePercentage =
+        totalLimits > 0
+          ? ((totalLimits - totalCredits) / totalLimits) * 100
+          : 0;
+
+      return availablePercentage.toFixed(2);
     } catch (error) {
       console.error("Error al obtener los créditos:", error);
-      return 0;
+      return 0; // En caso de error, retorna 0%
     }
   };
 
   const fetchData = async () => {
     try {
       const [voiceData, soundData] = await Promise.all([
-        fetch(`${process.env.REACT_APP_STATIC_URL}/voices-list.json`).then((res) =>
-          res.json()
+        fetch(`${process.env.REACT_APP_STATIC_URL}/voices-list.json`).then(
+          (res) => res.json()
         ),
-        fetch(`${process.env.REACT_APP_STATIC_URL}/sounds-list.json`).then((res) =>
-          res.json()
+        fetch(`${process.env.REACT_APP_STATIC_URL}/sounds-list.json`).then(
+          (res) => res.json()
         ),
       ]);
       setVoices(voiceData);
@@ -37,7 +71,12 @@ function TTS() {
   };
 
   useEffect(() => {
-    fetchData();
+    const fetchAndSetCredits = async () => {
+      const percentage = await fetchCredits();
+      setCreditsPercentage(percentage);
+    };
+
+    fetchAndSetCredits();
   }, []);
 
   const handlePlayAudio = (directory, file) => {
@@ -69,9 +108,17 @@ function TTS() {
     <div className="tts-container">
       <header>
         <div id="headerContent">
-          <img src="./static/resources/singgers.webp" alt="Logo" id="headerLogo" />
+          <img
+            src="./static/resources/singgers.webp"
+            alt="Logo"
+            id="headerLogo"
+          />
           <h1>TTSoft - Sonidos y Voces</h1>
-          <img src="./static/resources/singgers.webp" alt="Logo" id="headerLogo" />
+          <img
+            src="./static/resources/singgers.webp"
+            alt="Logo"
+            id="headerLogo"
+          />
         </div>
       </header>
       <main id="layout">
@@ -80,8 +127,9 @@ function TTS() {
             <section id="intro">
               <h2>Holiwi!</h2>
               <p>
-                En esta página puedes probar y usar voces y sonidos. En este recuadro, puedes validar tu texto y
-                comprobar las voces y audios.
+                En esta página puedes probar y usar voces y sonidos. En este
+                recuadro, puedes validar tu texto y comprobar las voces y
+                audios.
               </p>
               <div
                 id="textEditor"
@@ -95,12 +143,14 @@ function TTS() {
                 <div id="progressBarContainer">
                   <div
                     id="progressBar"
-                    style={{ width: `${(textEditorContent.length / characterLimit) * 100}%` }}
+                    style={{
+                      width: `${
+                        (textEditorContent.length / characterLimit) * 100
+                      }%`,
+                    }}
                   ></div>
                 </div>
-                <button id="copyButton" className="button-74">
-                  Copiar
-                </button>
+                <button id="copyButton" className="button-74"></button>
               </div>
             </section>
 
@@ -112,7 +162,12 @@ function TTS() {
                     <button
                       key={voice.id}
                       className="button-30"
-                      onClick={() => handlePlayAudio(process.env.REACT_APP_VOICES_DIR, voice.file)}
+                      onClick={() =>
+                        handlePlayAudio(
+                          process.env.REACT_APP_VOICES_DIR,
+                          voice.file
+                        )
+                      }
                     >
                       {voice.name}
                     </button>
@@ -128,7 +183,12 @@ function TTS() {
                   <button
                     key={sound.id}
                     className="button-30"
-                    onClick={() => handlePlayAudio(process.env.REACT_APP_SOUNDS_DIR, sound.file)}
+                    onClick={() =>
+                      handlePlayAudio(
+                        process.env.REACT_APP_SOUNDS_DIR,
+                        sound.file
+                      )
+                    }
                   >
                     {sound.name}
                   </button>
